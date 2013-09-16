30c4b7c8d485da477b04edc01200288adb288646
Marco Reis <marco.reis@tjdft.jus.br>
2013-09-16 00:33:52 -0300

    update

--- src/main/java/net/marcoreis/lucene/capitulo_02/BuscadorArquivosLocais.java
+++ src/main/java/net/marcoreis/lucene/capitulo_02/BuscadorArquivosLocais.java
@@ -1,4 +1,4 @@
-package net.marcoreis.lucene.capitulo_01;
+package net.marcoreis.lucene.capitulo_02;
 
 import java.io.File;
 
@@ -8,6 +8,7 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.Explanation;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
@@ -18,12 +19,14 @@ import org.apache.lucene.util.Version;
 
 public class BuscadorArquivosLocais {
   private static String diretorioIndice = System.getProperty("user.home")
-      + "/livro-lucene/indice-capitulo-01";
+      + "/livro-lucene/indice-capitulo-02";
   private static final Logger logger = Logger
       .getLogger(BuscadorArquivosLocais.class);
 
   public static void main(String[] args) {
-    new BuscadorArquivosLocais().buscar("java");
+    BuscadorArquivosLocais buscador = new BuscadorArquivosLocais();
+    String consulta = "conteudo:rafael~";
+    buscador.buscar(consulta);
   }
 
   public void buscar(String consulta) {
@@ -34,12 +37,14 @@ public class BuscadorArquivosLocais {
       //
       QueryParser parser = new QueryParser(Version.LUCENE_44, "",
           new StandardAnalyzer(Version.LUCENE_44));
-      Query query = parser.parse("conteudo:(" + consulta + ")");
+      Query query = parser.parse(consulta);
       TopDocs docs = buscador.search(query, 100);
+      Explanation explicacao = buscador.explain(query, 100);
       //
       for (ScoreDoc sd : docs.scoreDocs) {
         Document doc = buscador.doc(sd.doc);
-        logger.info("Caminho: " + doc.get("caminho"));
+        System.out.println("Arquivo: " + doc.get("dataAtualizacao") + " - "
+            + doc.get("caminho"));
       }
       //
       reader.close();
--- src/main/java/net/marcoreis/lucene/capitulo_02/IndexadorArquivosLocais.java
+++ src/main/java/net/marcoreis/lucene/capitulo_02/IndexadorArquivosLocais.java
@@ -1,17 +1,16 @@
-package net.marcoreis.lucene.capitulo_01;
+package net.marcoreis.lucene.capitulo_02;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.apache.log4j.Logger;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.document.Field;
 import org.apache.lucene.document.FieldType;
-import org.apache.lucene.document.StringField;
-import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.IndexWriterConfig.OpenMode;
@@ -24,7 +23,7 @@ public class IndexadorArquivosLocais {
   private static String diretorioDocumentosLocais = System
       .getProperty("user.home") + "/Dropbox/entrada";
   private static String diretorioIndice = System.getProperty("user.home")
-      + "/livro-lucene/indice-capitulo-01";
+      + "/livro-lucene/indice-capitulo-02";
   private static final Logger logger = Logger
       .getLogger(IndexadorArquivosLocais.class);
 
@@ -48,28 +47,28 @@ public class IndexadorArquivosLocais {
         if (!arquivo.isFile()) continue;
         try {
           Document doc = new Document();
-          String textoArquivo = extrator.parseToString(new FileInputStream(
-              arquivo));
-          doc.add(new TextField("conteudo", textoArquivo, Store.YES));
           FieldType tipo = new FieldType();
           tipo.setIndexed(true);
           tipo.setStored(true);
           tipo.setTokenized(true);
-          //doc.add(new Field("conteudo", textoArquivo, tipo ));
-          doc.add(new StringField("tamanho", String.valueOf(arquivo.length()),
-              Store.YES));
-          doc.add(new StringField("nome", arquivo.getAbsolutePath(), Store.YES));
-          doc.add(new StringField("dataAtualizacao", new Date(arquivo
-              .lastModified()).toString(), Store.YES));
-          doc.add(new StringField("caminho", arquivo.getAbsolutePath(),
-              Store.YES));
+          //
+          Date dataAtualizacao = new Date(arquivo.lastModified());
+          SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+          String dataFormatada = sdf.format(dataAtualizacao);
+          String textoArquivo = extrator.parseToString(new FileInputStream(
+              arquivo));
+          //
+          doc.add(new Field("conteudo", textoArquivo, tipo));
+          doc.add(new Field("tamanho", String.valueOf(arquivo.length()), tipo));
+          doc.add(new Field("dataAtualizacao", dataFormatada, tipo));
+          doc.add(new Field("caminho", arquivo.getAbsolutePath(), tipo));
+          doc.add(new Field("nome", arquivo.getName(), tipo));
           writer.addDocument(doc);
           logger.info("Arquivo indexado: " + arquivo.getAbsolutePath());
         } catch (Exception e) {
           logger.error("Não foi possível indexar o arquivo " + arquivo, e);
         }
       }
-      writer.commit();
       writer.close();
       diretorio.close();
       logger.info("Índice gerado com sucesso");
