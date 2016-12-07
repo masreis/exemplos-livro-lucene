e17e2ebc58b5d1b36378d6178ab5499e0a143c02
Marco Reis <ma@marcoreis.net>
2016-12-07 10:34:56 -0200

    update

--- src/test/java/net/marcoreis/lucene/capitulo_04/TesteBuscadorAPI.java
+++ src/test/java/net/marcoreis/lucene/capitulo_04/TesteBuscadorAPI.java
@@ -1,7 +1,18 @@
 package net.marcoreis.lucene.capitulo_04;
 
 import org.apache.log4j.Logger;
+import org.apache.lucene.document.LongPoint;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.FuzzyQuery;
+import org.apache.lucene.search.MatchAllDocsQuery;
+import org.apache.lucene.search.PhraseQuery;
+import org.apache.lucene.search.PrefixQuery;
 import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.WildcardQuery;
+import org.apache.lucene.search.spans.SpanNearQuery;
+import org.apache.lucene.search.spans.SpanQuery;
 import org.junit.Test;
 
 import net.marcoreis.lucene.capitulo_03.BuscadorArquivosLocais;
@@ -12,7 +23,42 @@ public class TesteBuscadorAPI {
 	@Test
 	public void testeQuery() {
 		BuscadorArquivosLocais buscador = new BuscadorArquivosLocais();
-		Query query = null;
+		//
+		MatchAllDocsQuery all = new MatchAllDocsQuery();
+		// NumericRangeQuery<Long> nrq =
+		// NumericRangeQuery.newLongRange("tamanho", UM_MEGA, DOIS_MEGAS,
+		// true, true);
+		Term term = new Term("conteudo", "extrair");
+		Query query = new TermQuery(term);
+		// PhraseQuery query = new PhraseQuery("conteudo", "ciência", "da",
+		// "informação");
+		Term[] termosAplicacaoExemplo = new Term[] { new Term("conteudo", "aplicação"),
+				new Term("conteudo", "exemplo") };
+		Term[] termosBancoDados = new Term[] { new Term("conteudo", "banco"), new Term("conteudo", "dados") };
+		//
+		// MultiPhraseQuery mpq = new MultiPhraseQuery();
+		// mpq.add(termosAplicacaoExemplo);
+		// mpq.add(termosBancoDados);
+		// mpq.setSlop(2);
+		//
+		Term termo = new Term("conteudo", "consider");
+		PrefixQuery pq = new PrefixQuery(termo);
+		// Term termo = new Term("conteudo", "consider");
+		SpanQuery query1;
+		SpanQuery[] clausulas = null;
+		SpanNearQuery sq = new SpanNearQuery(clausulas, 2, true);
+		BooleanQuery bq = null;// new BooleanQuery();
+		TermQuery tq = new TermQuery(new Term("conteudo", "calor"));
+		TermQuery tq1 = new TermQuery(new Term("conteudo", "java"));
+		// PhraseQuery pq = null;// new PhraseQuery();
+		// Term termo = new Term("conteudo", "calor");
+		WildcardQuery wq = new WildcardQuery(termo);
+		// Term termo = new Term("conteudo", "seção");
+		FuzzyQuery fq = new FuzzyQuery(termo, 2);
+		Query q = LongPoint.newRangeQuery("tamanhoLong", 8000, 40000);
+
+		//
+		// Query query = null;
 		buscador.buscar(query);
 	}
 
--- src/test/java/net/marcoreis/lucene/capitulo_04/TesteBuscadorSintaxeClassica.java
+++ src/test/java/net/marcoreis/lucene/capitulo_04/TesteBuscadorSintaxeClassica.java
@@ -3,29 +3,31 @@ package net.marcoreis.lucene.capitulo_04;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
+import net.marcoreis.lucene.capitulo_03.BuscadorArquivosLocais;
+
 public class TesteBuscadorSintaxeClassica {
 	private static final Logger logger = Logger.getLogger(TesteBuscadorSintaxeClassica.class);
 
-	@Test
+//	@Test
 	public void testeConsultaSintaxeClassica() {
 		logger.info("Sintaxe clássica");
 		String consulta = "conteudo:java";
-		BuscadorSintaxeClassica buscador = new BuscadorSintaxeClassica();
+		BuscadorArquivosLocais buscador = new BuscadorArquivosLocais();
 		buscador.buscar(consulta);
 	}
 
 	@Test
 	public void testeConsultaRegex() {
 		logger.info("Regex");
-		BuscadorSintaxeClassica buscador = new BuscadorSintaxeClassica();
+		BuscadorArquivosLocais buscador = new BuscadorArquivosLocais();
 		String regex = "";
 		regex = "/@abc~def@/"; // complemento
 		regex = "/@[0-9]{4}\\-[0-9]{4}@/"; // telefone
 		regex = "/@[2-9][0-9]{3}\\-[0-9]{4}@/"; // telefone BSB
 		regex = "/@[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}@/"; // IP
 		regex = "/@(0[1-9]|[1-2][0-9]|3[01])[\\- \\/\\.](0[1-9]|1[012])[\\- \\/\\.](19|20)[0-9][0-9]@/"; // Data
-		regex = "/.*bug.{1,5}[0-9]{4}@/"; // ”ug
 		regex = "/@[a-z0-9\\.\\_\\%\\+\\-]+\\@[a-z0-9\\.\\-]+\\.[a-z]{2,}@/"; // email
+		regex = "/.*bug.{1,5}[0-9]{4}@/"; // ”ug
 		String consulta = "conteudoNaoAnalisado:" + regex;
 		buscador.buscar(consulta);
 	}
